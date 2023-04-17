023aed7dd4421bdbed662c2799b8e50adcbfbf0d
RamonaNF <a01423596@tec.mx>
2023-04-17 14:03:42 -0600

    Add: 3/10

--- logic/Actividad_5.1.pl
+++ logic/Actividad_5.1.pl
@@ -8,3 +8,32 @@ lasto([_ | Tail], X, R) :-
 % lasto(Y, 5, 5).
 % lasto([], X, X).
 % lasto([1, 2, 3, 4], X, X).
+
+
+
+
+% (butlasto lst result)
+% Función lógica que tiene éxito si result contiene los mismos elementos que lst excepto el último
+
+butlasto([HEAD | []], []).
+butlasto([HEAD1 | TAIL1], [HEAD1 | TAIL]):-
+    butlasto(TAIL1, TAIL).
+
+% butlasto([1, 2, 3, 4], X).
+% butlasto(X, [1, 2, 3, 4]).
+% butlasto(X, Y).
+
+
+
+
+% (enlisto lst result)
+% Función lógica que tiene éxito si result contiene los mismos elementos que lst pero cada uno colacado dentro de una lista.
+
+enlisto([], []).
+enlisto([HEAD1 | TAIL1], [[HEAD1] | TAIL]):-
+    enlisto(TAIL1, TAIL).
+
+% enlisto([a, b, c, d, e], X).
+% enlisto(X, [a, b, c, d, e]).
+% enlisto(X, [[a], [b], [c], [d], [e]]).
+% enlisto(X, Y).
