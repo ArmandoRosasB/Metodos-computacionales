7197b6fc9f94f4665dbd6d3b5fbbe8cb26b34195
RamonaNF <a01423596@tec.mx>
2023-04-26 10:55:02 -0600

    progress: pizza nocturna

--- Paralelismo/Sincronizacion/Semaphore_Activity/Pizza_nocturna.cpp
+++ Paralelismo/Sincronizacion/Semaphore_Activity/Pizza_nocturna.cpp
@@ -30,6 +30,42 @@ pthread_cond_t space_available = PTHREAD_COND_INITIALIZER;
 
 int pizza_slices = 0;
 
+// Prototipos
+void comer();
+void preparar();
+void* leetcoders(void*);
+void* allNigthLong(void*);
+
 int main(int argc, char* argv[]) {
     return 0;
+}
+
+void comer() {
+    pizza_slices -= 1;
+}
+
+void preparar() {
+    pizza_slices = SIZE;
+} 
+
+void* leetcoders(void*) {
+    cout << "Coding time\n";
+    while(1) {
+        pthread_mutex_lock(&mutex);
+    }
+}
+
+void* allNigthLong(void*){
+    cout << "Delivering pizza...\n";
+    while(1) {
+        pthread_mutex_lock(&mutex);
+        if(pizza_slices > 0){
+            pthread_cond_wait(&space_available, &mutex);
+        }
+        preparar();
+        pthread_cond_signal(&pizza_available);
+        pthread_mutex_unlock(&mutex);
+    }
+    cout << "Pizza delivered.\nTime to code...\n\n";
+    pthread_exit(NULL);
 }
